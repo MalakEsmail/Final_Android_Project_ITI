abeceeceeadce85ad74ce27eabf68d93616174a4
malak <malakeltanekhey@gmail.com>
2020-10-13 16:30:11 +0200

    Reminder Dialog

--- app/src/main/java/com/example/mytrips/AddFragment.java
+++ app/src/main/java/com/example/mytrips/AddFragment.java
@@ -4,81 +4,89 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 
 public class AddFragment extends Fragment {
-    EditText etTripName,etStartPoint,etEndPoint;
-    Spinner repetitionSpinner,tripTypeSpinner;
-    Button btnCalendar,btnAlarm,btnAdd;
-    TextView tvCalendar,tvAlarm;
+    EditText etTripName, etStartPoint, etEndPoint;
+    Spinner repetitionSpinner, tripTypeSpinner;
+    Button btnCalendar, btnAlarm, btnAdd;
+    TextView tvCalendar, tvAlarm;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_add, container, false);
-        etTripName=view.findViewById(R.id.etTripName);
-        etStartPoint=view.findViewById(R.id.etStartPoint);
-        etEndPoint=view.findViewById(R.id.etEndPoint);
-        repetitionSpinner=view.findViewById(R.id.repetitionSpinner);
-        tripTypeSpinner=view.findViewById(R.id.tripTypeSpinner);
-        btnAlarm=view.findViewById(R.id.btnAlarm);
-        btnCalendar=view.findViewById(R.id.btnCalendar);
-        btnAdd=view.findViewById(R.id.btnAdd);
-        tvCalendar=view.findViewById(R.id.tvCalendar);
-        tvAlarm=view.findViewById(R.id.tvAlarm);
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+        etTripName = view.findViewById(R.id.etTripName);
+        etStartPoint = view.findViewById(R.id.etStartPoint);
+        etEndPoint = view.findViewById(R.id.etEndPoint);
+        repetitionSpinner = view.findViewById(R.id.repetitionSpinner);
+        tripTypeSpinner = view.findViewById(R.id.tripTypeSpinner);
+        btnAlarm = view.findViewById(R.id.btnAlarm);
+        btnCalendar = view.findViewById(R.id.btnCalendar);
+        btnAdd = view.findViewById(R.id.btnAdd);
+        tvCalendar = view.findViewById(R.id.tvCalendar);
+        tvAlarm = view.findViewById(R.id.tvAlarm);
+
 
 
         //Calendar btn
-        Calendar calendar=Calendar.getInstance();
-        final int year=calendar.get(Calendar.YEAR);
-        final int month=calendar.get(Calendar.MONTH);
-        final int day=calendar.get(Calendar.DAY_OF_MONTH);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
         btnCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DatePickerDialog datePickerDialog=new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                        month=month+1;
-                        String date=day+"/"+month+"/"+year;
+                        month = month + 1;
+                        String date = day + "/" + month + "/" + year;
                         tvCalendar.setText(date);
                     }
-                },year,month,day);
+                }, year, month, day);
                 datePickerDialog.show();
 
             }
         });
 
         //Alarm btn
-        Calendar c=Calendar.getInstance();
-        final int hour=calendar.get(c.HOUR_OF_DAY);
-        final int minute=calendar.get(c.MINUTE);
+        Calendar c = Calendar.getInstance();
+        final int hour = calendar.get(c.HOUR_OF_DAY);
+        final int minute = calendar.get(c.MINUTE);
         btnAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TimePickerDialog timePickerDialog=new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-                        String time=hour+":"+minute;
+                        String time = hour + ":" + minute;
                         tvAlarm.setText(time);
                     }
-                },hour,minute,true);
+                }, hour, minute, true);
                 timePickerDialog.show();
             }
         });
 
         return view;
     }
+
 }
