e07051cf21513231d83f8e2edf74ce09f1834d4f
Mariam <mariammostafa7799@gmail.com>
2020-10-13 22:25:51 +0300

    AddFragment database

---
+++ app/src/main/java/com/example/mytrips/TripInfo.java
@@ -0,0 +1,64 @@
+package com.example.mytrips;
+
+public class TripInfo {
+    private String name,startPoint,endPoint,time,date,tripType,repetition;
+
+    public TripInfo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(String startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTripType() {
+        return tripType;
+    }
+
+    public void setTripType(String tripType) {
+        this.tripType = tripType;
+    }
+
+    public String getRepetition() {
+        return repetition;
+    }
+
+    public void setRepetition(String repetition) {
+        this.repetition = repetition;
+    }
+}
